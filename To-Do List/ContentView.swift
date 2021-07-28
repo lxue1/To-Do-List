9ae84d7be79b30fce8d1f602f7419f44222bef4b
Luke Xue <lukexue064@gmail.com>
2021-07-28 10:41:04 -0500

    Initial Commit

---
+++ To-Do List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To-Do List
+//
+//  Created by Luke Xue on 7/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
